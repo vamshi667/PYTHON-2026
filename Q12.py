51f970f5b2f37efecd9fe82d00663722a68920c3
krishna vamshi <akrishnavamshi09@gmail.com>
2026-06-24 19:38:02 +0530

    Add attendance and assignment logic in Q12.py

---
+++ Q12.py
@@ -0,0 +1,11 @@
+attendance = True
+assignment_submitted = False
+
+# a. Operator to get True
+print(attendance or assignment_submitted)
+
+# b. Operator to get False
+print(attendance and assignment_submitted)
+
+# c. Operator to make attendance False
+print(not attendance)
