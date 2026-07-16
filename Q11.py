530767d21b8c0fcf74ed948544a50c7647616e66
krishna vamshi <akrishnavamshi09@gmail.com>
2026-07-16 19:25:21 +0530

    Update Q11.py

--- Q11.py
+++ Q11.py
@@ -1,27 +1,27 @@
 subjects = ["Python", "SQL", "Excel", "Tableau"]
 
-# a. Display complete list
+#11a. Display complete list
 print("Complete List:", subjects)
 
-# b. Display first and last subject
+#11b. Display first and last subject
 print("First Subject:", subjects[0])
 print("Last Subject:", subjects[-1])
 
-# c. Add one new subject between Python and SQL
+#11c. Add one new subject between Python and SQL
 subjects.insert(1, "Power BI")
 print("Updated List:", subjects)
 
-# d. Delete Excel
+#11d. Delete Excel
 subjects.remove("Excel")
 print("After Removing Excel:", subjects)
 
-# e. Copy list into another list
+#11e. Copy list into another list
 new_list = subjects.copy()
 print("Copied List:", new_list)
 
-# f. Sort the new list
+#11f. Sort the new list
 new_list.sort()
 print("Sorted List:", new_list)
 
-# g. Check if Excel is present
+#11g. Check if Excel is present
 print("Is Excel Present?", "Excel" in subjects)
